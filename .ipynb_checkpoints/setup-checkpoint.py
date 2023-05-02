7448b0760e20f69e0753848769c7455124de627f
Isidro Pride <122856334+Isidro5004@users.noreply.github.com>
2023-05-02 19:42:10 -0400

    Delete .ipynb_checkpoints directory

--- .ipynb_checkpoints/setup-checkpoint.py
+++
@@ -1,8 +0,0 @@
-import setuptools
-setuptools.setup(name = 'casino',
-    version = '1.0',
-    url = 'https://github.com/Isidro5004/Casino',
-    author = 'Isidro Pride',
-    author_email = 'zxv6mt@virginia.edu',
-    license = 'MIT',
-    packages=['casino', 'tests'])
